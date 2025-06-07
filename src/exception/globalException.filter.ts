fa1ded6ae0a988199486c915282ae9821627a302
blurbeast <leumasre@gmail.com>
2025-06-08 00:55:36 +0100

    few ti

--- src/exception/globalException.filter.ts
+++ src/exception/globalException.filter.ts
@@ -12,11 +12,19 @@ export class GlobalExceptionFilter implements ExceptionFilter {
   // private readonly logger = new Logger(GlobalExceptionFilter.name);
   catch(exception: unknown, host: ArgumentsHost): void {
     const caughtException = host.switchToHttp();
+
+    // response and request objects
+    // instance of the response to send out the error
     const response = caughtException.getResponse<Response>();
+
+    // instance of the request to get the request details
     const request = caughtException.getRequest<Request>();
 
+    // get the appropriate status code of the the exception thrown e.g 400, 404 
+    // or 500 if it internal error
     const status =
       exception instanceof HttpException ? exception.getStatus() : 500;
+    // get the message thrown from the exception object
     const message =
       exception instanceof HttpException
         ? exception.message
@@ -39,6 +47,8 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     // );
 
 
+
+    // a json response to send out showing the full error details
     response.status(status).json({
       statusCode: status,
       message: message,
